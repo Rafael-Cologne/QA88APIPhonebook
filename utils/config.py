7ffa11825e798e8c269db6255e8a22400fad4630
Rafael Babaew <rafaelbabaew@gmail.com>
2026-09-09 13:37:47 +0200

    Initial commit

---
+++ utils/config.py
@@ -0,0 +1,4 @@
+BASE_URL = "https://contactapp-telran-backend.herokuapp.com" # константы пишутся большими буквами
+API_VERSION = "/v1"
+REGISTRATION_URL = "/user/registration/usernamepassword"
+LOGIN_URL = "/user/login/usernamepassword"
